27fd79220603667e83855c366f4792b11ed346d7
nick-ccc <nicholas.mastand@gmail.com>
2026-01-16 19:38:32 -0500

    Fix options k3s

--- modules/k3s/default.nix
+++ modules/k3s/default.nix
@@ -1,11 +1,23 @@
 { config, lib, pkgs, ... }:
+
+with lib;
+
 {
+  options.server.enable = mkOption {
+    type = types.bool;
+    default = false;
+    description = "Enable server role for K3s";
+  };
+
+  options.agent.enable = mkOption {
+    type = types.bool;
+    default = false;
+    description = "Enable agent role for K3s";
+  };
+
   # Import both server and agent modules unconditionally
   imports = [
     ./server.nix
     ./agent.nix
   ];
-
-  server.enable = lib.mkDefault true;
-  agent.enable = lib.mkDefault true;
 }
